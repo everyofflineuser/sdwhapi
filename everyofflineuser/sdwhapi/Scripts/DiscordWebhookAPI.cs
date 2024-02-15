448abd98e602a04da8ae265a46f398d892a9ceff
Tochno Ne Aleks <113923625+NeAleks18@users.noreply.github.com>
2024-02-16 01:46:52 +0900

    Create DiscordWebhookAPI.cs

---
+++ everyofflineuser/sdwhapi/Scripts/DiscordWebhookAPI.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+public class DiscordWebhookAPI : MonoBehaviour
+{
+    private string WebHookID = "ID";
+    private string WebHookToken = "TOKEN";
+    public string verAPI { get; private set; } = "1.1";
+ 
+
+    string url;
+
+    private void Awake()
+    {
+        url = $"https://discord.com/api/webhooks/{WebHookID}/{WebHookToken}";
+    }
+
+    public void SendMessage(bool debug, string content, string username = null, string avatar_url = null, bool tts = false)
+    {
+        var request = WebRequest.Create(url);
+        request.Method = "POST";
+
+        ExecuteWebhookObject execute = new ExecuteWebhookObject();
+        if (content != null) execute.content = content;
+        if (username != null) execute.username = username;
+        if (avatar_url != null) execute.avatar_url = avatar_url;
+        if (tts != false) execute.tts = tts;
+
+        var json = JsonConvert.SerializeObject(execute);
+        byte[] byteArray = Encoding.UTF8.GetBytes(json);
+
+        request.ContentType = "application/json";
+        request.ContentLength = byteArray.Length;
+
+        using var reqStream = request.GetRequestStream();
+        reqStream.Write(byteArray, 0, byteArray.Length);
+
+        using var response = request.GetResponse();
+        if (debug) Debug.Log(((HttpWebResponse)response).StatusDescription);
+
+        using var respStream = response.GetResponseStream();
+
+        using var reader = new StreamReader(respStream);
+        string data = reader.ReadToEnd();
+        if (debug) Debug.Log(data);
+    }
+
+    public string GetMessage(string msgid)
+    {
+        var request = WebRequest.Create(url + $"/messages/{msgid}");
+        request.Method = "GET";
+
+        using var webResponse = request.GetResponse();
+        using var webStream = webResponse.GetResponseStream();
+
+        using var reader = new StreamReader(webStream);
+        var data = reader.ReadToEnd();
+        return data.ToString();
+    }
+
+    public void EditMessage(bool debug, string msgid, string content)
+    {
+        var request = WebRequest.Create(url + $"/messages/{msgid}");
+        request.Method = "PATCH";
+
+        ExecuteWebhookObject execute = new ExecuteWebhookObject();
+        if (content != null) execute.content = content;
+
+        var json = JsonConvert.SerializeObject(execute);
+        byte[] byteArray = Encoding.UTF8.GetBytes(json);
+
+        request.ContentType = "application/json";
+        request.ContentLength = byteArray.Length;
+
+        using var reqStream = request.GetRequestStream();
+        reqStream.Write(byteArray, 0, byteArray.Length);
+
+        using var response = request.GetResponse();
+        if (debug) Debug.Log(((HttpWebResponse)response).StatusDescription);
+
+        using var respStream = response.GetResponseStream();
+
+        using var reader = new StreamReader(respStream);
+        string data = reader.ReadToEnd();
+        if (debug) Debug.Log(data);
+    }
+
+    public void DeleteMessage(bool debug, string msgid)
+    {
+        var request = WebRequest.Create(url + $"/messages/{msgid}");
+        request.Method = "DELETE";
+
+        using var webResponse = request.GetResponse();
+        using var webStream = webResponse.GetResponseStream();
+
+        using var reader = new StreamReader(webStream);
+        var data = reader.ReadToEnd();
+        if (debug) Debug.Log(data + "\n" + ((HttpWebResponse)webResponse).StatusDescription);
+    }
+
+}
+
+public class ExecuteWebhookObject
+{
+    public string content;
+    public string username;
+    public string avatar_url;
+    public bool tts;
+}
